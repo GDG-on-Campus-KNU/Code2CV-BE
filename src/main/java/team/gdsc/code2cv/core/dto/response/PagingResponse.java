abae4b5954910f703a347c4033d53ce18e308bd6
kiryanchi <kiryanchi@gmail.com>
2024-05-06 00:58:49 +0900

    Violations 수정

--- src/main/java/team/gdsc/code2cv/core/dto/response/PagingResponse.java
+++ src/main/java/team/gdsc/code2cv/core/dto/response/PagingResponse.java
@@ -1,32 +1,33 @@
 package team.gdsc.code2cv.core.dto.response;
 
-import lombok.Builder;
-import org.springframework.data.domain.Page;
-
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+
+import lombok.Builder;
+
 @Builder
 public record PagingResponse<T>(
-        Integer totalPage,
-        List<T> data
+	Integer totalPage,
+	List<T> data
 ) {
-    public static <T> PagingResponse<T> of(List<T> data, Integer totalPage) {
-        return PagingResponse.<T>builder()
-                .totalPage(totalPage)
-                .data(data)
-                .build();
-    }
+	public static <T> PagingResponse<T> of(List<T> data, Integer totalPage) {
+		return PagingResponse.<T>builder()
+			.totalPage(totalPage)
+			.data(data)
+			.build();
+	}
 
-    public static <Entity, Model> PagingResponse<Model> from(Page<Entity> data, Function<Entity, Model> converter) {
-        List<Model> convertedData = data.getContent().stream()
-                .map(converter)
-                .collect(Collectors.toList());
+	public static <Entity, Model> PagingResponse<Model> from(Page<Entity> data, Function<Entity, Model> converter) {
+		List<Model> convertedData = data.getContent().stream()
+			.map(converter)
+			.collect(Collectors.toList());
 
-        return PagingResponse.<Model>builder()
-                .totalPage(data.getTotalPages())
-                .data(convertedData)
-                .build();
-    }
+		return PagingResponse.<Model>builder()
+			.totalPage(data.getTotalPages())
+			.data(convertedData)
+			.build();
+	}
 }
--- src/main/java/team/gdsc/code2cv/core/jwt/Role.java
+++ src/main/java/team/gdsc/code2cv/core/jwt/Role.java
@@ -1,21 +1,21 @@
 package team.gdsc.code2cv.core.jwt;
 
+import java.util.Set;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
-import java.util.Set;
 
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    USER("ROLE_USER"),
-    ADMIN("ROLE_ADMIN");
-    private final String value;
+	USER("ROLE_USER"),
+	ADMIN("ROLE_ADMIN");
+	private final String value;
 
-    public Set<Role> getRoles() {
-        return switch (this) {
-            case USER -> Set.of(Role.USER);
-            case ADMIN -> Set.of(Role.USER, Role.ADMIN);
-        };
-    }
+	public Set<Role> getRoles() {
+		return switch (this) {
+			case USER -> Set.of(Role.USER);
+			case ADMIN -> Set.of(Role.USER, Role.ADMIN);
+		};
+	}
 }
