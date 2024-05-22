77ed780b454edfa32852448c85d74ff86adf68c2
yooonwodyd <dbswodyd00@naver.com>
2024-05-22 18:21:39 +0900

    [FIX]: Validaotr의 isValid 메서드가 enum의 검증 메서드를 사용하도록 변경

--- src/main/java/team/gdsc/code2cv/feature/user/domain/VendorNameValidator.java
+++ src/main/java/team/gdsc/code2cv/feature/user/domain/VendorNameValidator.java
@@ -1,7 +1,5 @@
 package team.gdsc.code2cv.feature.user.domain;
 
-import java.util.Arrays;
-
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import team.gdsc.code2cv.feature.user.entity.VendorName;
@@ -16,13 +14,6 @@ public class VendorNameValidator implements ConstraintValidator<ValidVendorName,
 		if (value == null) {
 			return true; // `null`을 유효한 것으로 간주하거나 `false`로 설정하면 안 됨
 		}
-		try {
-			Arrays.stream(VendorName.values())
-				.map(VendorName::getName)
-				.anyMatch(s -> s.equals(value));
-			return true;
-		} catch (IllegalArgumentException e) {
-			return false;
-		}
+		return VendorName.valid(value);
 	}
 }
