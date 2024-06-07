0c2a2bd548e37cdb23c9ba98b3e1c1d331f89e0e
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-06-07 23:25:07 +0900

    [Fix]: join_column 추가

--- src/main/java/team/gdsc/code2cv/feature/project/entity/ProjectAnalysisCommitMessage.java
+++ src/main/java/team/gdsc/code2cv/feature/project/entity/ProjectAnalysisCommitMessage.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -20,6 +21,7 @@ public class ProjectAnalysisCommitMessage {
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "project_analysis_id")
 	private ProjectAnalysis projectAnalysis;
 
 	private String commitMessageEntire;
