f548f64a5c81e0aa3b8dc740ceeac653e289d3b7
손홍석 <78216059+bayy1216@users.noreply.github.com>
2024-06-25 16:40:03 +0900

    [Feat]: 프로젝트 컨트롤러 작성 (#26)

---
+++ src/main/java/team/gdsc/code2cv/feature/project/controller/ProjectController.java
@@ -0,0 +1,32 @@
+package team.gdsc.code2cv.feature.project.controller;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "Project", description = "프로젝트 관련 API")
+@RestController
+@RequiredArgsConstructor
+public class ProjectController {
+
+
+	@Operation(summary = "프로젝트 목록 조회", description = "사용자의 프로젝트 목록 전체 조회")
+	@GetMapping("/api/projects")
+	public List<Objects> getAllProjects() {
+		throw new UnsupportedOperationException("아직 구현되지 않았습니다.");
+
+	}
+
+	@Operation(summary = "프로젝트 분석", description = "사용자 프로젝트를 갱신합니다.")
+	@PostMapping("/api/projects/analyze")
+	public void analyzeProject() {
+
+	}
+}
